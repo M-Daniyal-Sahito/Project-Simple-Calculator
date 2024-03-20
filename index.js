0bddee8ca781b5c0b7529cfab0af37259e467eb9
M Daniyal Sahito <160993870+M-Daniyal-Sahito@users.noreply.github.com>
2024-03-20 14:29:10 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,64 @@
-import inquirer from "inquirer";
-const answer = await inquirer.prompt([
-    { message: "first enter number", type: "number", name: "firstNumber" },
-    { message: "seconde enter number", type: "number", name: "secondeNumber" },
-    {
-        message: "Select one of the operator to perform operation ",
-        type: "list",
-        name: "operator",
-        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
-    },
-]);
-// condition statement 
-if (answer.operator === "Addition") {
-    console.log(answer.firstNumber + answer.secondeNumber);
+// function funcname():string{
+//     let name="Daniyal"
+//     return(name)
+// }
+// let nmae:string=funcname()
+// console.log(nmae);
+// function with if and else statement
+// function canEat(age: number): string {
+//     if(age>=20){
+//         return "yes, You can eat this"
+//     }
+//     else if (age>15){
+//         return "No, You can'nt eat this"
+//     }
+//     else {
+//         return "When you are 20 so you can eat this"
+//     }
+// }
+// let old:string=canEat(0)
+// console.log(old);
+//Example2 
+// function isCommon(fruit:string): string | undefined{
+//     if (fruit==="apple" || fruit==="banana"){
+//         return `Yes, ${fruit} is a common fruit.`;
+//     }
+//     else {
+//         `No, ${fruit} is not common`;
+//     }
+// }
+// let result:string |undefined =isCommon("banana");
+// // console.log(result);
+// result=isCommon("orange")
+// console.log(result);
+//  function checkFruit(fruit: string): string | undefined {
+//     if (fruit === "apple" || fruit === "banana" || fruit === "orange") {
+//         return `Yes, ${fruit} is a common fruit.`;
+//      } else {         return `No, ${fruit} is not a common fruit.`;     }
+//  }
+//  let result: string | undefined = checkFruit("banana");
+// console.log(result);
+//  result = checkFruit("pine");
+//  console.log(result);
+//  function isCommon(fruit:string):string | undefined{
+//      if (fruit === "apple" || fruit === "mango" || fruit === "orange") {
+//          return `Yes ${fruit} is common.`;
+//      } else { return `No, ${fruit } is not common`}
+//  }
+//  let result:string | undefined= isCommon("apple")
+//  console.log(result);
+//  result= isCommon("ana")
+//  console.log(result);
+//Example-3
+function isPass(marks) {
+    if (marks >= 90 || marks >= 80 || marks >= 70 || marks >= 60) {
+        return "The marks is ".concat(marks, " so student is pass");
+    }
+    else {
+        return "the marks is ".concat(marks, " so student is fail ");
+    }
 }
-else if (answer.operator === "Subtraction") {
-    console.log(answer.firstNumber - answer.secondeNumber);
-}
-else if (answer.operator === "Multiplication") {
-    console.log(answer.firstNumber * answer.secondeNumber);
-}
-else if (answer.operator === "Division") {
-    console.log(answer.firstNumber / answer.secondeNumber);
-}
-else {
-    console.log("Please Enter Valid Operation");
-}
+var result = isPass(60);
+console.log(result);
+result = isPass(40);
+console.log(result);
--- index.ts
+++ index.ts
@@ -1,32 +1,51 @@
-// making simple command prompt calculator
-
-import inquirer from "inquirer";
-
-const answer = await inquirer.prompt([
-  { message: "first enter number", type: "number", name: "firstNumber" },
-  { message: "seconde enter number", type: "number", name: "secondeNumber" },
-  {
-    message: "Select one of the operator to perform operation ",
-    type: "list",
-    name: "operator",
-    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
-  },
-]);
- // condition statement 
- if (answer.operator === "Addition"){
-  console.log(answer.firstNumber + answer.secondeNumber);
+// example1
+// function funcname():string{
+//     let name="Daniyal"
+//     return(name)
+// }
+// let nmae:string=funcname()
+// console.log(nmae);
+// function with if and else statement
+//example 2
+// function canEat(age: number): string {
+//     if(age>=20){
+//         return "yes, You can eat this"
+//     }
+//     else if (age>15){
+//         return "No, You can'nt eat this"
+//     }
+//     else {
+//         return "When you are 20 so you can eat this"
+//     }
+// }
+// let old:string=canEat(0)
+// console.log(old);
+//Example3 
+// function isCommon(fruit:string): string | undefined{
+//     if (fruit==="apple" || fruit==="banana"){
+//         return `Yes, ${fruit} is a common fruit.`;
+//     }
+//     else {
+//         `No, ${fruit} is not common`;
+//     }
+// }
+// let result:string |undefined =isCommon("banana");
+// // console.log(result);
+//exmaple4//  function checkFruit(fruit: string): string | undefined {
+//     if (fruit === "apple" || fruit === "banana" || fruit === "orange") {
+//         return `Yes, ${fruit} is a common fruit.`;
+//      } else {         return `No, ${fruit} is not a common fruit.`;     }
+//  }
+//  let result: string | undefined = checkFruit("banana");
+// console.log(result);
+// example 5
+function isPass(marks:number):string | undefined {
+    if (marks >= 90 || marks >= 80 || marks >=70 || marks >= 60 ){return `The marks is ${marks} so student is pass`
 }
-else if (answer.operator === "Subtraction"){
-  console.log(answer.firstNumber - answer.secondeNumber);
+else{return `the marks is ${marks} so student is fail `}
 }
-else if (answer.operator === "Multiplication"){
-  console.log(answer.firstNumber * answer.secondeNumber);
-}
-else if (answer.operator === "Division"){
-  console.log(answer.firstNumber / answer.secondeNumber);
-}
-else {
-  console.log("Please Enter Valid Operation" );}
-
-
+let result:string | undefined=isPass(60)
+console.log(result);
+result= isPass(40)
+console.log(result);
 
